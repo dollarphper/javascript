b904bf5c341dc05f89fd93c9e623ae0673c84f7b
lee <complet@163.com>
2018-06-27 12:02:20 +0800

    无限级分类添加深度

--- gettree.js
+++ gettree.js
@@ -3,14 +3,16 @@
  @param data 需要转换的数据
  @param pkey 上级的键名
  @param key 主键名
- @param pid 顶级id
+ @param pid 顶级id 默认0
+ @param depth 深度 默认1
  @return ret 转换后的数组
  */
-function gettree(data,pkey,key,pid = 0){
+function gettree(data,pkey,key,pid = 0,depth = 1){
 	this.data = data
 	this.pkey = pkey
 	this.key = key
 	this.pid = pid
+	this.depth = depth
 	/*
 	 @desc：获取梯式分类
 	 @param level 起始层级 默认0
@@ -20,14 +22,17 @@ function gettree(data,pkey,key,pid = 0){
 		var pkey = this.pkey
 		var key = this.key
 		var pid = this.pid
+		var depth = this.depth
 	    var ret = new Array()
-	    for (var i in data){
-	    	if(data[i][pkey] == pid){
-	    		data[i]['level'] = level
-	    		ret.push(data[i])
-	    		this.pid = data[i][key]
-	    		ret = ret.concat(this.getleveltree(level + 1))
-	    	}
+	    if(level < depth){
+		    for (var i in data){
+		    	if(data[i][pkey] == pid){
+		    		data[i]['level'] = level
+		    		ret.push(data[i])
+		    		this.pid = data[i][key]
+		    		ret = ret.concat(this.getleveltree(level + 1))
+		    	}
+		    }
 	    }
 	    return ret
 	}
@@ -39,12 +44,17 @@ function gettree(data,pkey,key,pid = 0){
 		var pkey = this.pkey
 		var key = this.key
 		var pid = this.pid
+		var depth = this.depth
 		var ret = new Array()
-		for (var i in data){
-			if(data[i][pkey] == pid){
-				this.pid = data[i][key]
-				data[i]['child'] = this.getsubtree()
-				ret.push(data[i])
+		if(depth > 0){
+			depth --
+			this.depth = depth		
+			for (var i in data){
+				if(data[i][pkey] == pid){
+					this.pid = data[i][key]
+					data[i]['child'] = this.getsubtree()
+					ret.push(data[i])
+				}
 			}
 		}
 		return ret
@@ -65,6 +75,6 @@ function gettree(data,pkey,key,pid = 0){
 // 		}
 // 	)
 // var gettree = new gettree(data,'pid','id')
-// var ret = gettree.getleveltree()
+// var ret = gettree.getsubtree()
 // console.log(ret)
 module.exports = gettree
